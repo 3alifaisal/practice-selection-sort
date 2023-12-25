62d3e79d74fa53d93ef78b0ce375f2fcc3c0500d
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2023-12-25 23:31:48 +0100

    finished selection
    need to go back to understand shuffling more
    mid 40 min

--- selection-sort.js
+++ selection-sort.js
@@ -3,44 +3,70 @@
 function selectionSort(arr) {
 
   // Copy the original array
+  let arrCopy  = arr.slice()
 
   // Create an array to store the sorted values
-
+  let sorted  = [];
   // While the array is not empty...
-
+  while(arrCopy.length !== 0){
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
-
-    // Save and remove the value at the min index
-
+    let min  = arrCopy[0];
+    let minInd = 0;
+    for(let i = 1; i < arrCopy.length ; i++) {
+      if(min > arrCopy[i]){
+        min = arrCopy[i];
+        minInd = i;
+      }
+    }
+     // Save and remove the value at the min index
+    const valueAtMinInd = arrCopy.splice(minInd,1)[0]
     // Add the min value to the end of the sorted array
-
+    sorted.push(valueAtMinInd);
+  }
+  return sorted;
 }
 
-
+selectionSort([31,45,21,52,112])
 
 function selectionSortInPlace(arr) {
+  const n = arr.length;
 
-  // Set a pointer at zero diving the array into sorted and unsorted halves
-
-  // Repeat while the unsorted half is not empty:
-
-    // Do not move this console.log
+  for (let i = 0; i < n - 1; i++) {
     console.log(arr.join(","));
 
-    // Find the index of the minimum value in the unsorted half
+    let minIndex = i;
 
-    // Save the min value
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
 
-    // Shift every unsorted value to the left of the min value to the right by 1
+    const minValue = arr[minIndex];
 
-    // Put the min value at the divider
+    // Shift all elements between minIndex and i forward by one place
+    for (let k = minIndex; k > i; k--) {
+      arr[k] = arr[k - 1];
+    }
 
-    // Increment the divider and repeat
+    // Place the minimum value at the correct position
+    arr[i] = minValue;
+  }
 
+  console.log(arr.join(","));
 }
+
+// // Example usage
+// const arrayToSort = [2, 4, 6, 8, 1, 3, 5, 7, 9];
+// selectionSortWithShift(arrayToSort);
+// console.log("Sorted array:", arrayToSort);
+
+
+// Example usage
+
 
 
 module.exports = [selectionSort, selectionSortInPlace];
